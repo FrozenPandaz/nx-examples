3f53b59c936cb514022ddd0b5e8c20047a9ef852
Jason Jean <jasonjean1993@gmail.com>
2020-05-23 22:00:52 -0400

    show error

--- apps/products/src/app/app.component.ts
+++ apps/products/src/app/app.component.ts
@@ -8,3 +8,11 @@ import '@nx-example/shared/header';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {}
+
+// Before:
+// VSCode will think this is okay because jest is in tsconfig.json
+// Webstorm will somehow know to use tsconfig.app.json
+
+// After:
+// Both IDEs know to use tsconfig.app.json for this file and error.
+describe('hi', () => {});
